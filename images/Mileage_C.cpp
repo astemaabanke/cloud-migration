82593844c34094cc4ee553bd4cb33446a98e9d34
Alex Tema <alextemaabanke@gmail.com>
2024-05-21 14:18:06 -0400

    all-school-website

---
+++ images/Mileage_C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int MileDriven[0];
+    int GallonsUsed[0];
+    int MPG[0];
+    int TotalMPG[0];
+    
+    
+    cout<<"Enter miles driven (-1 to quit):";
+    while ( MilesDriven != -1) {
+
+    cin>> MilesDriven;
+    cout<< "Enter gallos used";
+    cin>> GallonsUsed;
+
+    }
+    
+
+    return 0;
+}
